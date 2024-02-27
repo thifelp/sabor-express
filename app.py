14c1f59bf4175b16680b7fa7c9dc93c6ab42d1e6
thifelp <thifelp@gmail.com>
2024-02-26 23:10:50 -0300

    Manipulação de Strings

---
+++ app.py
@@ -0,0 +1,10 @@
+print('Ｓａｂｏｒ Ｅｘｐｒｅｓｓ 🥗 \n\n')
+
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair \n')
+
+escolha = input('Selecione uma das opções: ')
+print('Você escolheu a opção: ', escolha + '°')
